06619301576a31c2e2a7e223f0dd1caa343ea0af
matan.broit <matan.broit@appsflyer.com>
2019-05-23 21:17:12 +0300

    search works

--- sw_app/application.py
+++ sw_app/application.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, flash
 from werkzeug import secure_filename
 from ..transcribe import transcribe
+import requests
 import uuid
+import json
 import os
 
 
@@ -34,5 +36,20 @@ def search_testimonies():
 
 @app.route('/results', methods=['GET', 'POST'])
 def search_results():
-    search_line = request.form['search_line']
-    return render_template('results.html',search_line=search_line)
+    search_term = request.form['search_line']
+    url = "https://soulwings.search.windows.net/indexes/azureblob-index/docs"
+    headers = {
+      "api-key": "F01491A07C517D0BD467CAA8AEF2FE35",
+      "Content-Type": "application/json"
+    }
+    params = {
+      "api-version": "2019-05-06",
+      "search": search_term
+    }
+    response_json = requests.get(url, params=params, headers=headers).json()
+    search_results = response_json['value']
+    number_of_results = len(search_results)
+    files_uuid = [result['metadata_storage_name'].split(".")[0] for result in search_results]
+    video_url = "https://soulwings.blob.core.windows.net/videos/"
+    files_urls = [video_url + file_uuid + ".mp4" for file_uuid in files_uuid]
+    return render_template('results.html',search_line=files_urls)
