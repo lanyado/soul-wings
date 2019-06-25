f8bd3f90df73d47b48911a3374afa145cf64b602
GalSomekh <gal@panoply.io>
2019-06-25 12:11:56 -0400

    Added config

---
+++ config.py
@@ -0,0 +1,11 @@
+
+import os
+
+REPO_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+WORKING_DIR = '{}/working_dir'.format(REPO_DIRECTORY)
+
+S3_BUCKET = 'soul-wings'
+
+GCS_BUCKET = 'soul-wings'
+
+DEFAULT_LANG = 'hebrew'
